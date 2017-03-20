684867dc40581f9e4268c5b7c17d967f327eb64e
handaoui-mohamed <handaoui.mohamed@gmail.com>
2017-03-20 11:23:01 +0100

    Added swagger documentation and improved uploads routes

--- app/upload/models.py
+++ app/upload/models.py
@@ -1,18 +1,17 @@
-import os
-from app import db, app
+from app import db
 from config import HOST_URL, UPLOAD_FOLDER
 
 # file upload
 class PostUpload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.String, db.ForeignKey('post.id'))
+    user_id = db.Column(db.String, db.ForeignKey('user.id'))
 
     def to_json(self):
         return {
             'id': self.id,
-            'path': os.path.join(HOST_URL, UPLOAD_FOLDER, 'post', str(self.post_id), self.name).replace("\\", "/"),
+            'path': HOST_URL+'/posts/'+UPLOAD_FOLDER+"/"+self.post_id+"?filename="+self.name,
             'name': self.name
         }
 
@@ -21,12 +20,12 @@ class PostUpload(db.Model):
 class ProfilePicture(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.String, db.ForeignKey('user.id'))
 
     def to_json(self, username):
         return {
             'id': self.id,
-            'path': os.path.join(HOST_URL, UPLOAD_FOLDER, 'profile', username, self.name).replace("\\", "/"),
+            'path': HOST_URL+'/profile/'+UPLOAD_FOLDER+"?username="+username+"&filename="+self.name,
             'name': self.name,
             'user_id': self.user_id
         }
@@ -35,12 +34,12 @@ class ProfilePicture(db.Model):
 class LegalDocument(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.String, db.ForeignKey('user.id'))
 
     def to_json(self, username):
         return {
             'id': self.id,
-            'path': os.path.join(HOST_URL, UPLOAD_FOLDER, 'legaldocument', username, self.name).replace("\\", "/"),
+            'path': HOST_URL+'/legaldocument/'+UPLOAD_FOLDER+"?username="+username+"&filename="+self.name,
             'name': self.name,
             'user_id': self.user_id
         }
