f774668cc195175fb00b623785bc543f982b7357
Handaoui Mohamed <handaoui.mohamed@gmail.com>
2017-03-20 14:51:15 +0100

    fixed users id in message

--- app/message/models.py
+++ app/message/models.py
@@ -2,8 +2,8 @@ from app import db
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    sender_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    receiver_id = db.Column(db.Integer)
+    sender_id = db.Column(db.String, db.ForeignKey("user.id"))
+    receiver_id = db.Column(db.String)
     content = db.Column(db.String(200))
     sent_at = db.Column(db.DateTime)
  
